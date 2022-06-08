d7f635ff64fbe0860f7b39e9e282ba354513ce78
Bounty-Strike <105740825+Bounty-Strike@users.noreply.github.com>
2022-06-08 13:07:42 -0400

    Create FlingScript.lua

---
+++ FlingScript.lua
@@ -0,0 +1,7 @@
+local Fling = function(User)
+  local plr = game:GetService("Players").LocalPlayer
+  
+  if plr.Character and plr.Character:FindFirstChild("Humanoid") then
+    return game:Exit()
+    end
+  end
