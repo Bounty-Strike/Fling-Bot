2dba2302e591c2bde6872caf489a114eb05fb1d7
Bounty-Strike <105740825+Bounty-Strike@users.noreply.github.com>
2022-06-08 13:02:38 -0400

    Update ReturnPlayer.lua

--- ReturnPlayer.lua
+++ ReturnPlayer.lua
@@ -1,1 +1,15 @@
+local GetUser = function(Player)
+    local plrs = game:GetService("Players"):GetPlayers()
+    local Players = {}
+    if Player then
+        for _, v in next, plrs do
+            if v ~= LocalPlayer then
+                table.insert(Players, v)
+            end
+        end
+        return Players
+    end
+    return nil
+end
 
+return GetUser
